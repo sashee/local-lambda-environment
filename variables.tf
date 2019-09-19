4f72300f13d9d3b1f17264b8603b6e31a118d01b
Tamas Sallai <gsashee@gmail.com>
2019-09-19 13:31:40 +0200

    Add default Lambda assume role policy

--- variables.tf
+++ variables.tf
@@ -1,5 +1,19 @@
 variable "policy" {
   type = "string"
+	default = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
 }
 
 variable "dev_mode" {
